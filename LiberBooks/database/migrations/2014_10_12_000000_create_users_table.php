1b4881b8e45d8b09c426ec50737ec9a434cb3292
sacred142 <agung.alfatah43@gmail.com>
2022-11-28 16:29:59 +0700

    menyelesaikan fitur login dengan google dan facebook

--- LiberBooks/database/migrations/2014_10_12_000000_create_users_table.php
+++ LiberBooks/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
+            $table->string('google_id')->nullable();
+            $table->string('facebook_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
