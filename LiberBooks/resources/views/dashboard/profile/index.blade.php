9cba0fbe018c37bf93406adf4adc0de4ccef57f5
sacred142 <agung.alfatah43@gmail.com>
2022-12-26 16:20:10 +0700

    penyesuaian

--- LiberBooks/resources/views/dashboard/profile/index.blade.php
+++ LiberBooks/resources/views/dashboard/profile/index.blade.php
@@ -31,8 +31,7 @@
               @endif
               <div class="space-y-2">
                 <div class="text-lg leading-6 font-medium space-y-1">
-                  <h3>Renandra Rahadian Putri</h3>
-                  <p class="text-indigo-600">Pembaca</p>
+                  <h3 class="text-center">{{ $user->name }}</h3>
                 </div>
                 <ul role="list" class="flex justify-center space-x-5">
                   <li>
